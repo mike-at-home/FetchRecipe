ba19a8a64ea5b1d9df24f69720ba621a624bd4ff
Mike Kasianowicz <mikekasiano@gmail.com>
2025-01-14 14:33:12 -0600

    Initial Commit

---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Michael Kasianowicz on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipeTests/FetchRecipeTests.swift
@@ -0,0 +1,16 @@
+//
+//  FetchRecipeTests.swift
+//  FetchRecipeTests
+//
+//  Created by Michael Kasianowicz on 1/14/25.
+//
+
+import Testing
+
+struct FetchRecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
